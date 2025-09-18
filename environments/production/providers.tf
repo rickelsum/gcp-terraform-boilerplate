c65215de73fb88077f54fa4f8d634656b9516c2c
Rick Elsum <rick@1sourcesystems.com.au>
2025-09-19 00:49:15 +0800

    Initial Push

---
+++ environments/production/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  # This will use the credentials from the gcloud console session
+}
